c474dfda24be08bb24191ae52fc15b1674924616
KISELEVAA <mikeown@protonmail.com>
2021-09-24 03:03:54 +0300

    main

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { contextIsolated } = require('process')
+const { Client } = require('ssh2');
+const { Pool } = require('pg');
 
 function createWindow () {
+
     const win = new BrowserWindow({
       width: 720,
       height: 820,
@@ -18,8 +21,53 @@ function createWindow () {
     // Load Login_form
     win.loadFile('login.html')
     //win.loadFile('index.html')
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
+
     
+    
+    const conn = new Client();
+    conn.on('ready', () => {
+      console.log('Client :: ready');
+      conn.exec('uptime', (err, stream) => {
+        if (err) throw err;
+        stream.on('close', (code, signal) => {
+          console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+          conn.end();
+        }).on('data', (data) => {
+          console.log('STDOUT: ' + data);
+        }).stderr.on('data', (data) => {
+          console.log('STDERR: ' + data);
+        });
+      });
+    }).connect({
+      host: '192.168.1.11',
+      port: 22,
+      username: 'artem',
+      password:'5381'
+    });
+    
+
+    var config = {
+      user: 'postgres', 
+      database: 'postgres', 
+      password: '5381', 
+      host: '127.0.0.0', 
+      port: 5433, 
+      max: 10,
+      idleTimeoutMillis: 0,
+      connectionTimeoutMillis: 0
+      };
+
+    const pool = new Pool(config);
+    pool.on('error', function (err, client) {
+      console.error('idle client error', err.message, err.stack);
+    });
+    pool.query('SELECT $1::int AS number', ['2'], function(err, res) {
+      if(err) {
+          return console.error('error running query', err);
+      }
+      console.log('number:', res.rows[0].number);
+    });
   }
 
   app.whenReady().then(() => {
